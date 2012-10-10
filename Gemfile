a9fb6e97e91fa604450811b85d271863cc00bd4c
Ryan Taylor Long <ryan@rtlong.com>
2012-10-10 03:20:58 -0500

    Add Tika JAR and some code

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem 'nokogiri'
+gem 'pry'
--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 # app.rb
 require 'sinatra'
+require 'nokogiri'
+require 'pry'
 
 get '/' do
-  'Hello world!'
+  haml :index
+end
+post '/' do
+  url = params['url']
+
+  results = Nokogiri::XML(`java -jar ./tika-app-1.2.jar -x "#{url}"`)
+  metadata = results.css('meta').map{|n| [n['name'], n['content']] }
+  text  = results.css('body').text
+
+  haml :results, locals: { metadata: metadata, text: text, url: url }
 end
